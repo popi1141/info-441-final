3f2be8c628fd422731f23937e6ef4e096f60ecc2
Thomas Serrano <serrat@uw.edu>
2021-12-07 13:44:03 -0800

    fixed the case of the missing clown 2

--- src/components/ClownsonaSection.js
+++ src/components/ClownsonaSection.js
@@ -8,6 +8,7 @@ function ClownsonaSection({data}) {
       <h2> This is your current clownsona, {data.name}. Your clownsona is clownsona {data.number}! </h2> 
       <h3> Change your clownsona by going <Link href="/settings/clownsona" passHref={true}> here </Link> </h3>
       {(data.number === "1") && <img className={"logoPic"} src="/clownsonas/clown1.svg" alt="work pls" />}
+      {(data.number === "2") && <img className={"logoPic"} src="/clownsonas/clown3.svg" alt="Logo" />}
       {(data.number === "3") && <img className={"logoPic"} src="/clownsonas/clown3.svg" alt="Logo" />}
       {(data.number === "4") && <img className={"logoPic"} src="/clownsonas/clown4.svg" alt="Logo" />}
       {(data.number === "5") && <img className={"logoPic"} src="/clownsonas/clown5.svg" alt="Logo" />}
