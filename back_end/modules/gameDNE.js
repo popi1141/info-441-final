ebe7df74f5eee8e55fc28182d5e028bcf3026f02
Thomas Serrano <serrat@uw.edu>
2021-12-07 16:42:03 -0800

    re structure the file system into two repos

--- back_end/modules/gameDNE.js
+++
@@ -1,5 +0,0 @@
-// sends that the game does not exist to the user and then disconnects their socket
-export default function gameDNE(socket, msg) {
-    socket.emit("fatal", msg)
-    socket.disconnect(0)
-}
