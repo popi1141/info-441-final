549b5744926473cf43ce892a436eff9a3998997d
Thomas Serrano <serrat@uw.edu>
2021-12-07 16:45:06 -0800

    oops, undelete files

---
+++ src/pages/auth/[type].js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Meta from "components/Meta";
+import AuthSection from "components/AuthSection";
+
+function AuthPage(props) {
+  const router = useRouter();
+
+  return (
+    <>
+      <Meta title="Auth" />
+      <AuthSection
+        bg="white"
+        textColor="dark"
+        size="md"
+        bgImage=""
+        bgImageOpacity={1}
+        type={router.query.type}
+        providers={["google", "facebook", "twitter"]}
+        afterAuthPath={router.query.next || "/"}
+      />
+    </>
+  );
+}
+
+export const getStaticPaths = () => ({
+  paths: [
+    { params: { type: "signin" } },
+    { params: { type: "signup" } },
+    { params: { type: "forgotpass" } },
+    { params: { type: "changepass" } },
+  ],
+  fallback: true,
+});
+
+export function getStaticProps({ params }) {
+  return { props: {} };
+}
+
+export default AuthPage;
